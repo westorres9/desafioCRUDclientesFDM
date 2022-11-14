e7836b7e34826a4229cba747f22e32bebb1b3f1f
westorres9 <wester.torres9@gmail.com>
2022-11-14 08:55:23 -0300

    desafioCRUDcliente iniciado

---
+++ desafioCRUDcliente/src/main/java/com/devsuperior/desafioCRUDcliente/DesafioCruDclienteApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.desafioCRUDcliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioCruDclienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioCruDclienteApplication.class, args);
+	}
+
+}
---
+++ desafioCRUDcliente/src/test/java/com/devsuperior/desafioCRUDcliente/DesafioCruDclienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.desafioCRUDcliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioCruDclienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
